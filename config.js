74ef0518375ab06d9e0987c484aa129210ca8806
nabiljouahry14-tech <nabiljouahry14@gmail.com>
2026-02-28 17:57:44 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+export const SUPABASE_URL = "https://bshhjnihovkeiokxacnp.supabase.co";
+export const SUPABASE_ANON_KEY = "sb_publishable_FVatWtv9a7IkhklQNUkIlw_CeibbDog";
+
+/* add this line */
+export const STORAGE_BUCKET = "ticket-attachments";
